d4260e62061ecc202496950619c8a96d1691ff00
Tiffany Valentine <52210944+devWitch-ctrl@users.noreply.github.com>
2020-08-31 20:25:49 -0600

    Add files via upload
    First Commit

---
+++ js_stopwatch.js
@@ -0,0 +1,59 @@
+
+const minutes = 00;
+const seconds = 00;
+const tens = 00;
+const appendMins = document.getElementById("minutes");
+const appendTens = document.getElementById("tens");
+const appendSeconds = document.getElementById("seconds");
+const buttonStart = document.getElementById("button-start");
+const buttonStop = document.getElementById("button-stop");
+const buttonReset = document.getElementById("button-reset");
+const Interval = ;
+
+buttonStart.onclick = function() {
+  clearInterval(Interval);
+  Interval = setInterval(startTimer, 10);
+};
+
+buttonStop.onclick = function() {
+  clearInterval(Interval);
+};
+
+buttonReset.onclick = function() {
+  clearInterval(Interval);
+  minutes = 00 : ;
+  seconds = 00 : ;
+  seconds = 00;
+  appendMinutes.innerHTML = minutes;
+  appendTens.innerHTML = tens;
+  appendSeconds.innterHTML = seconds;
+};
+
+
+function startTimer () {
+  tens++;
+
+  if(tens < 9){
+    appendTens.innerHTML = "0" + tens;
+  }
+
+  if (tens > 9){
+    appendTens.innerHTML = tens;
+
+  }
+
+  if (tens > 99) {
+    console.log("seconds");
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + 0;
+  }
+
+  if (seconds > 9){
+    appendSeconds.innerHTML = seconds;
+  }
+
+};
+
+startTimer();
