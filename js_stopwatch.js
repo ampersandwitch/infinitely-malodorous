6e2b6d3b7e240b65ca652ac99c9e5d717534c968
Tiffany Valentine <52210944+devWitch-ctrl@users.noreply.github.com>
2020-09-01 16:36:42 -0600

    Add files via upload
    Completed

--- js_stopwatch.js
+++ js_stopwatch.js
@@ -1,59 +1,75 @@
+window.onload = function() {
 
-const minutes = 00;
-const seconds = 00;
-const tens = 00;
-const appendMins = document.getElementById("minutes");
-const appendTens = document.getElementById("tens");
-const appendSeconds = document.getElementById("seconds");
-const buttonStart = document.getElementById("button-start");
-const buttonStop = document.getElementById("button-stop");
-const buttonReset = document.getElementById("button-reset");
-const Interval = ;
+  let minutes = 00;
+  let seconds = 00;
+  let tens = 00;
 
-buttonStart.onclick = function() {
-  clearInterval(Interval);
-  Interval = setInterval(startTimer, 10);
-};
+  const appendMinutes = document.getElementById("minutes");
+  const appendSeconds = document.getElementById("seconds")
+  const appendTens = document.getElementById("tens")
 
-buttonStop.onclick = function() {
-  clearInterval(Interval);
-};
+  const buttonStart = document.getElementById('button-start');
+  const buttonStop = document.getElementById('button-stop');
+  const buttonReset = document.getElementById('button-reset');
+  let Interval;
 
-buttonReset.onclick = function() {
-  clearInterval(Interval);
-  minutes = 00 : ;
-  seconds = 00 : ;
-  seconds = 00;
-  appendMinutes.innerHTML = minutes;
-  appendTens.innerHTML = tens;
-  appendSeconds.innterHTML = seconds;
-};
+//buttons
+
+  buttonStart.onclick = function() {
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
+  };
+
+  buttonStop.onclick = function() {
+    clearInterval(Interval);
+  };
 
 
-function startTimer () {
-  tens++;
-
-  if(tens < 9){
-    appendTens.innerHTML = "0" + tens;
-  }
-
-  if (tens > 9){
+  buttonReset.onclick = function() {
+    clearInterval(Interval);
+    let minutes = "00";
+    let seconds = "00";
+    let tens = "00";
     appendTens.innerHTML = tens;
-
-  }
-
-  if (tens > 99) {
-    console.log("seconds");
-    seconds++;
-    appendSeconds.innerHTML = "0" + seconds;
-    tens = 0;
-    appendTens.innerHTML = "0" + 0;
-  }
-
-  if (seconds > 9){
     appendSeconds.innerHTML = seconds;
+    appendMinutes.innerHTML = minutes;
+  };
+
+
+
+  function startTimer() {
+
+    // tenth of a second
+    tens++;
+    if (tens < 9) {
+      appendTens.innerHTML = "0" + tens;
+    }
+    if (tens > 9) {
+      appendTens.innerHTML = tens;
+
+    }
+    if (tens > 99) {
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    };
+
+    //seconds
+    if (seconds <= 9) {
+      appendSeconds.innterHTML = "0" + seconds;
+    }
+    if (seconds > 9) {
+      appendSeconds.innerHTML = seconds;
+    }
+    if (seconds > 59) {
+      console.log("minutes");
+      minutes++;
+      appendMinutes.innerHTML = "0" + minutes;
+      seconds = 0;
+      appendSeconds.innerHTML = "0" + 0;
+    };
   }
 
 };
-
-startTimer();
